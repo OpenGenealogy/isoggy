0e6d00758801ca769b14dabf9d64da206e5d9497
Rob Hoare <bitbucket@robhoare.com>
2014-02-17 14:13:27 -0700

    commented code

--- readgroup.php
+++ readgroup.php
@@ -2,27 +2,25 @@
 // extract haplogroup tree data.
 // Copyright 2014 Rob Hoare.  License: MIT.
 // https://github.com/OpenGenealogy/isoggy
-$version = '0.1.1';
-$page = file_get_contents('php://stdin');
-preg_match_all("~<!(.*?)<br>~s",$page,$matches);
-foreach($matches[0] as $match) {
-	$match = str_replace('&nbsp;','',$match);
-	$match = str_replace('&nbsp','',$match);	
-	$match = str_replace(chr(149),'',$match);
-	$match = str_replace("\n",'',$match);
-	$match = str_replace("\r",'',$match);
-	preg_match_all("~>(.*?)<~s",$match,$entries);
-	$entry = join(' ',$entries[1]);
+$version = '0.1.2';
+$page = file_get_contents('php://stdin'); // read group file from stdin
+preg_match_all("~<!(.*?)<br>~s",$page,$matches); // select detail lines
+foreach($matches[0] as $match) { // for each set of details
+	$match = str_replace('&nbsp;','',$match); // remove non-br spaces
+	$match = str_replace('&nbsp','',$match); // and incomplete ones	
+	$match = str_replace(chr(149),'',$match); // remove bullets
+	$match = str_replace("\n",'',$match); // remove new lines
+	$match = str_replace("\r",'',$match); // remove returns
+	preg_match_all("~>(.*?)<~s",$match,$entries); // remove all html
+	$entry = join(' ',$entries[1]); // join up what's left, with spaces
 	if (!strstr($entry,'font-weight:')) { // skip stylesheet
-		$entry = trim(str_replace('">','',$entry));
-		$e = split(' ',$entry,2);
-		$snp = trim(array_pop($e));
-		$sc = trim($e[0]);
+		$entry = trim(str_replace('">','',$entry)); // fix untidy A
+		list($sc,$snp) = explode(' ',$entry,2); // split subclade, snp
 		$snp = str_replace('/S9 PF605','/S9,PF605',$snp); // fix R
-		$snp = str_replace(' ','',$snp);
-		$snp = str_replace('>','',$snp);
+		$snp = str_replace(' ','',$snp); // remove spaces
+		$snp = str_replace('>','',$snp); // remove stray tag closings
 		if ($sc != 'Y') { // ignore Y on A file		
-			echo "$sc|$snp\n";
+			echo "$sc|$snp\n"; // write to stdout
 		};
 	};
 };
--- readindex.php
+++ readindex.php
@@ -2,27 +2,27 @@
 // extract ISOGG Y-DNA SNP Index 
 // Copyright 2014 Rob Hoare.  License: MIT.  
 // https://github.com/OpenGenealogy/isoggy	
-$version = '0.1.1';
-$page = file_get_contents('php://stdin');
-preg_match_all("~<tr>(.*?)</tr>~s",$page,$matches);
-foreach($matches[0] as $match) {
-	$match = str_replace('&nbsp;','',$match);	
-	$match = str_replace("\n",'',$match);
-	preg_match_all("~<td>(.*?)</td>~s",$match,$f);
-	foreach($f[1] as $field) {
-		if (strstr($field,'<a href=')) { // remove url
-			preg_match_all("~>(.*?)<~s",$field,$text);
-			$field = $text[1][0];
-		};
-		$field = str_replace("-&gt;",'->',$field);
-		$l[] = trim($field);		
+$version = '0.1.2';
+$page = file_get_contents('php://stdin'); // read index file from stdin
+preg_match_all("~<tr>(.*?)</tr>~s",$page,$matches); // find table rows
+foreach($matches[0] as $match) { // for each yable row
+	$match = str_replace('&nbsp;','',$match); // remove non-br spaces	
+	$match = str_replace("\n",'',$match); // remove new lines
+	preg_match_all("~<td>(.*?)</td>~s",$match,$f); // find table cells
+	foreach($f[1] as $field) { // for each table cell
+		if (strstr($field,'<a href=')) { // if there is an url ...
+			preg_match_all("~>(.*?)<~s",$field,$text); // select text
+			$field = $text[1][0]; // set field to text without url
+		};		
+		$field = str_replace("-&gt;",'->',$field); // replace html code
+		$l[] = trim($field); // add to array of field		
 	};
-	$line = join('|',$l);
-	if (!strstr($line,'font size')) { // skip top
-		if (!strstr($line,'Other Names')) { // skip heading
-			echo "$line\n";
+	$line = join('|',$l); // join up the fields with | delimiter
+	if (!strstr($line,'font size')) { // skip top of page
+		if (!strstr($line,'Other Names')) { // skip table heading
+			echo "$line\n"; // write to stdout
 		};	
 	};	
-	$l = '';
+	$l = ''; // empty the field array before next line
 };		
 exit;
